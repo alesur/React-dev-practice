cb98130f9af82db50c6ffd861e8870172d2f99a4
alesur <aleksandr.surkov@outlook.com>
2019-11-04 17:33:47 +0200

    Java Spring Rest service

---
+++ restfull-web-service/src/main/java/io/github/alesur/todoapp/restfullwebservice/RestfullWebServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.alesur.todoapp.restfullwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullWebServiceApplication.class, args);
+	}
+
+}
